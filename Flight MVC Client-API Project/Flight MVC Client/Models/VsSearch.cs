7d8d58fde16ad7d9e30ffac10484811b62b32be0
Vaibhav Srivastava <99118025+VaibhavSr007@users.noreply.github.com>
2024-01-24 15:46:43 -0500

    Final Client Side Commit

---
+++ Flight MVC Client-API Project/Flight MVC Client/Models/VsSearch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace flightProject.Models;
+
+public partial class VsSearch
+{
+    public int SearchId { get; set; }
+
+    [Required(ErrorMessage = "*")]
+    [Display(Name = "Desination")]
+    public string? SearchDest { get; set; }
+
+    [Required(ErrorMessage = "*")]
+    [Display(Name = "Source")]
+    public string? SearchSrc { get; set; }
+}
---
+++ Flight MVC Client-API Project/Flight MVC Client/Models/flights.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace flightProject.Models;
+
+public partial class flights
+{
+
+    public int FlightId { get; set; }
+
+    [Required(ErrorMessage = "*")]
+    [Display(Name = "Flight Name")]
+    public string? FlightName { get; set; }
+
+    [Required(ErrorMessage = "*")]
+    [Display(Name = "Source")]
+    public string? Src { get; set; }
+
+    [Display(Name = "Destination")]
+    [Required(ErrorMessage = "*")]
+    public string? Dest { get; set; }
+
+    [Required(ErrorMessage = "*")]
+    [Display(Name = "Price per Ticket")]
+    public int? Rate { get; set; }
+
+}
