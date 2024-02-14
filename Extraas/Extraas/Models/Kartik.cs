b2fe4bc35ca862e8267fd300efbe007cc0826389
Vaibhav Srivastava <99118025+VaibhavSr007@users.noreply.github.com>
2024-02-14 18:21:33 +0530

    Add files via upload

---
+++ Extraas/Extraas/Models/Kartik.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EfcoreEx.Models;
+
+public partial class Kartik
+{
+    public int Sid { get; set; }
+
+    public string? Sname { get; set; }
+
+    public double? Marks { get; set; }
+}
